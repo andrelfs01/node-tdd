4f9d7b604cb7acddf01687034c38dc16fac1b643
andrelfs01 <andre.luifs01@gmail.com>
2023-08-21 00:50:34 -0300

    'iniciando'

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "estudo_js",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    logging: false,
+    define: {
+      timestamps : true,
+      underscored:  true,
+      underscoredAll:  true,
+    },
+};
